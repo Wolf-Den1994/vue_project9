5948a404bf363184b8042ffb0cc629d793b4353b
Denis <wolf-den@tut.by>
2022-01-01 17:46:33 +0300

    dinamic query

--- src/components/AppEmailBody.vue
+++ src/components/AppEmailBody.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <h2>Тема письма</h2>
+  <div v-if="email">
+    <h2>{{ email.theme }}</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi, cumque ea laborum laudantium nostrum odio quis soluta sunt velit?</p>
-    <button class="btn">Закрыть</button>
+    <button class="btn" @click="$router.push('/mail/')">Закрыть</button>
   </div>
+  <div v-else>Выберите письмо</div>
 </template>
 
 <script>
 export default {
+  inject: ['emails'],
+  props: ['mailId'],
+  computed: {
+    email() {
+      return this.emails.find(e => e.id === +this.mailId)
+    }
+  }
 }
 </script>
 
